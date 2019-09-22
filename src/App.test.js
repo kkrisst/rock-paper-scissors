c6f2409af088a827936bc2e523d0ef96524539cc
Elisabetta Barta <kkrisst@gmail.com>
2019-09-22 19:18:12 +0200

    Added some App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme'
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('Testing the App component', () => {
+
+  const wrapper = shallow(<App />);
+
+  it('expect MenuPage to be defined', () => {
+    expect(App).toBeDefined();
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('expect MenuPage to match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+})
