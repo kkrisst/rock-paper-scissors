a79215c53e846f649c2f7b32120589160aeded01
Elisabetta Barta <kkrisst@gmail.com>
2019-09-22 22:44:18 +0200

    src/

--- src/pages/cpu-page/cpu-page.component.jsx
+++ src/pages/cpu-page/cpu-page.component.jsx
@@ -1,14 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import PlayerChoices from '../../components/player-choices/player-choices.component.jsx';
 
 import './cpu-page.styles.scss';
 
-const CPUPage = () => {
+class CPUPage extends Component {
 
-  return (
-    <div className='cpu-page'>
-      CPU Page
-    </div>
-  )
+  constructor(props){
+    super(props);
+
+    this.state = {
+      waitingForChoice: true,
+      playerItem: '',
+      cpuItem: '',
+      results: 1 // 1: player won, 2: cpu won, 3: draw
+    }
+  };
+
+  onChoice = playerItem => {
+    if (this.state.waitingForChoice) {
+      const cpuItem = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
+      let results = 1;
+      
+      if (playerItem === cpuItem) {
+        results = 3;
+      } else if ( (cpuItem === 'rock' && playerItem === 'scissors') || 
+            (cpuItem === 'paper' && playerItem === 'rock') ||
+            (cpuItem === 'scissors' && playerItem === 'paper')
+      ) {
+        results = 2;
+      }
+
+      console.log(playerItem);
+      console.log(cpuItem);
+      console.log(results);
+
+      this.setState({
+        waitingForChoice: false,
+        playerItem,
+        cpuItem,
+        results
+      });
+    }
+  }
+
+  startNewGame = () => {
+    this.setState({
+      waitingForChoice: true,
+      playerItem: '',
+      cpuItem: '',
+      results: 1
+    });
+  }
+
+  renderResults() {
+    switch (this.state.results) {
+      case 1:
+        return <div className='results-value'>You won!</div>;
+      case 2:
+        return <div className='results-value'>You lost!</div>;
+      case 3:
+        return <div className='results-value'>Draw!</div>;
+      default:
+        return '';
+    }
+  }
+
+  render () {
+    const { waitingForChoice, playerItem, cpuItem } = this.state;
+
+    return (
+      <div className='cpu-page'>
+        <div className='title'>Player item</div>
+        <PlayerChoices handleChoice={this.onChoice} interactive={waitingForChoice} selected={playerItem}/>
+        {
+          waitingForChoice
+          ? (<div className='resultsContainer'>Choose your item to see the results!</div>)
+          : (
+            <div className='resultsContainer'>
+              { this.renderResults() }
+              <div className='new-game-button' onClick={this.startNewGame}>New Game</div>
+            </div>
+          )
+        }
+        <PlayerChoices handleChoice={() => {}} interactive={false} selected={cpuItem} />
+        <div className='title'>CPU item</div>
+      </div>
+    )
+  }
+  
 };
 
 export default CPUPage;
